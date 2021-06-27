622a122b8780866b5d4689f5b46c05397355c7b4
nikhil chavan <chavannikhil1997@gmail.com>
2021-06-27 11:14:04 +0530

    setup hiroku app

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 const { response } = require('express')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -94,6 +95,6 @@ app.get('*', (req, res) =>{
     })
 })
 
-app.listen(3000, () =>{
-    console.log('web server is running on 3000 port')
+app.listen(port, () =>{
+    console.log('web server is running on ' + port + ' port')
 })
